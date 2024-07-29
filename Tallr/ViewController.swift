d089c2625308ffaf7e566dfe726736454183aa26
github-felipe-borges <felipe.borges@swile.com.br>
2024-07-29 10:51:37 -0300

    Initial Commit

---
+++ Tallr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tallr
+//
+//  Created by Felipe Borges on 29/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
